9f898b7cfe76b90e7180534168dfcb88f3ebb52b
Misha Kosticyn <mishakosticyn@yandex.ru>
2017-09-19 19:02:58 +0300

    MT interface

---
+++ Program.fs
@@ -0,0 +1,8 @@
+﻿// types : MT
+type letterOfAlphabet = char
+type exSymbol = char
+type state = int
+type trackSymbol = TLetter of letterOfAlphabet | ExSymbol of exSymbol
+type shift = Right | Left
+type deltaFunc = Map<state * trackSymbol, state * trackSymbol * shift>
+type MT = state Set * letterOfAlphabet Set * trackSymbol Set * deltaFunc * state * state Set
