9ed29e49ba32e31bf47e8e678ef3065fb7eb3986
Misha Kosticyn <mishakosticyn@yandex.ru>
2017-12-05 20:59:20 +0300

    Main function was done

--- Main.fs
+++ Main.fs
@@ -1,7 +1,15 @@
 namespace MT
+open System.IO
 module Main =
+    let writeToFile file (str : string) =
+        use streamWriter = new StreamWriter(file, false)
+        streamWriter.Write(str)
     [<EntryPoint>]
     let main _ =
-        MTToGrammarZero.transformation BuildMT.PrimesMT
-        |> printfn "%A\n"
+        do MTToGrammarZero.transformation BuildMT.PrimesMT
+        |> ToString.grammarToString
+        |> writeToFile "grammarZero.txt"
+        do LBAToGrammarOne.transformation BuildMT.PrimesLBA
+        |> ToString.grammarToString
+        |> writeToFile "grammarOne.txt"
         0
